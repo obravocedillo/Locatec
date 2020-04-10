cf16f657433b2d1b5e2c39be4ec60e2faf03265e
Kenshex7 <hector12x13@gmail.com>
2020-04-09 20:46:23 -0500

    Creación de grids y objetos en la página de catálogo, creación de un filtro de orden (Ambos sólo front)
    Signed-off-by: Kenshex7 <hector12x13@gmail.com>

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -3,6 +3,8 @@ import { LocatecContext } from '../Context/LocatecContext';
 import '../Style/Home.css'
 import Grid from '@material-ui/core/Grid';
 import NavBar from './NavBar'
+import ObjectCard from './ObjectCard'
+import OrderObjects from './OrderObjects'
 
 
 
@@ -26,9 +28,49 @@ class Catalog extends Component{
 
   render() {
     return (
-      <Grid container>
-        <NavBar></NavBar>
-      </Grid>
+        <Grid container>
+            <NavBar></NavBar>
+            <Grid container spacing={2}>
+                <Grid item xl={1}>
+                </Grid>
+                <Grid container spacing={2} xl={11}>
+                    <Grid item>
+                        <OrderObjects>
+
+                        </OrderObjects>
+                    </Grid>
+                    <Grid item>
+                        <Grid container justify="left" spacing={1}>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                            <Grid item>
+                                <ObjectCard>
+                                </ObjectCard>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+        
     )
   }
 }
---
+++ src/Components/ObjectCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 300,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require("../Assets/stories.jpg")}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Objeto Perdido
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Pequeña descripción del objeto para mayor información al usuario.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Ver más
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
